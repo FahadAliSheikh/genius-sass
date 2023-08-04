d4bc5798bb415c2ae8991a0c4196726e20c8bebf
FahadAliSheikh <fahad.a.sheikh@gmail.com>
2023-08-04 18:58:17 +0500

    conversation model using opnai api completed

---
+++ components/Loader.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+export const Loader = () => {
+  return (
+    <div className="h-full  flex flex-col gap-y-4 items-center justify-center">
+      <div className="relative h-10 w-10 animate-spin">
+        <Image alt="logo" fill src="/logo.png" />
+      </div>
+      <p className="text-muted-foreground text-sm text-center">
+        Genius is thinking...
+      </p>
+    </div>
+  );
+};
